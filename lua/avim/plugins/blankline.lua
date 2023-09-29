ad23005f8d0ec16a7739b307bc8ac450bd6134a3
Ano Rebel <hacker4rebel@gmail>
2023-09-29 12:36:48 +0300

    Changelog: - Updated indent-blankline config after v3

--- lua/avim/plugins/blankline.lua
+++ lua/avim/plugins/blankline.lua
@@ -1,69 +1,30 @@
 local M = {
 	"lukas-reineke/indent-blankline.nvim",
-	event = "BufRead",
+	-- event = "BufRead",
+	main = "ibl",
 }
 
 local options = {
-	use_treesitter = true,
-	show_current_context = true,
-	show_current_context_start = true,
-	indentLine_enabled = 1,
 	char = "▏",
-	filetype_exclude = {
-		"help",
-		"terminal",
-		"alpha",
-		"packer",
-		"lazy",
-		"lspinfo",
-		"TelescopePrompt",
-		"TelescopeResults",
-		"lsp-installer",
-		"",
+	exclude = {
+		--  filetypes = {
+		--  "help",
+		--  "terminal",
+		--  "alpha",
+		--  "packer",
+		--  "lazy",
+		--  "lspinfo",
+		--  "TelescopePrompt",
+		--  "TelescopeResults",
+		--  "lsp-installer",
+		--  "",
+		-- },
+		-- buftypes = { "terminal", "nofile" },
 	},
-	buftype_exclude = { "terminal", "nofile" },
-	show_trailing_blankline_indent = false,
-	show_first_indent_level = false,
-	-- context_highlight_list = { "Blue" },
-	context_patterns = {
-		-- NOTE: indent-blankline's defaults
-		"class",
-		"function",
-		"return",
-		"^func",
-		"method",
-		"^if",
-		"while",
-		"^while",
-		"for",
-		"with",
-		"try",
-		"except",
-		"arguments",
-		"argument_list",
-		"object",
-		"dictionary",
-		"element",
-		"table",
-		"tuple",
-		"block",
-		"if_statement",
-		"else_clause",
-		"catch_clause",
-		"try_statement",
-		"import_statement",
-		"operation_type",
-		-- NOTE: better Javascript/Typescript support
-		"return_statement",
-		"statement_block",
-	},
-	char_highlight_list = { "VertSplit" },
-	-- NOTE: alternating indentation highlight
-	space_char_hughlight_list = { "MsgSeparator", "Normal" },
 }
 
 function M.config()
-	require("indent_blankline").setup(options)
+	require("ibl").setup(options)
 end
 
 return M
